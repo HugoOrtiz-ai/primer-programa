abb431269b88e74452dd739ea2afa54f2f8cb797
Hugo Ortiz <personals_ervices@hotmail.com>
2025-05-09 02:42:41 -0600

    Actualiza el Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello world!\nBienvenid@ a Screenmatch!!!");
+        System.out.println("Película: Matrix");
+
+        int fechaDeLanzamiento = 1999;
+        boolean incluidoEnElPlan = true;
+        double notaDeLaPelicula = 8.2;
+
+        double media = (8.2 + 6.0 + 9.0) / 3;
+        System.out.println(media);
+
+        String sinopsis = """
+                Matrix es una paradoja.
+                La mejor Película del fin del milenio.
+                Fue lanzada en : 
+                """ + fechaDeLanzamiento;
+        System.out.println(sinopsis);
     }
 }
