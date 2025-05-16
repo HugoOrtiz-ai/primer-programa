7978f1feec70dbcc5c49add1e3a5f935d597529a
Hugo Ortiz <personals_ervices@hotmail.com>
2025-05-16 01:22:46 -0600

    Actualiza el Decisiones.java

--- src/Decisiones.java
+++ src/Decisiones.java
@@ -3,11 +3,18 @@ public class Decisiones {
         int fechaDeLanzamiento = 1999;
         boolean incluidoEnElPlan = true;
         double notaDeLaPelicula = 8.2;
+        String tipoPlan = "Plus";
 
         if (fechaDeLanzamiento >= 2022) {
             System.out.println("Películas más populares.");
         } else {
-            System.out.println("Películas Retro exitosas.");
+            System.out.println("Películas Retro que aún vale la pena ver.");
+        }
+
+        if (incluidoEnElPlan && tipoPlan.equals("Plus")) {
+            System.out.println("Disfrute de su película.");
+        }  else {
+            System.out.println("Película no disponible para su plan actual");
         }
     }
 }
