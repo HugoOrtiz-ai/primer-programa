01726f9488bed7b87b88f0772d031d5ee2472638
Hugo Ortiz <personals_ervices@hotmail.com>
2025-05-20 11:03:26 -0600

    Crea el Evaluaciones.java

---
+++ src/Evaluaciones.java
@@ -0,0 +1,24 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Evaluaciones {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        teclado.useLocale(Locale.US);
+
+        double nota = 0;
+        double mediaEvaluaciones = 0;
+        double totalEvaluaciones = 0;
+
+        while (nota != -1) {
+            System.out.println("Escribe la nota que le darías a la película Matrix:");
+            nota = teclado.nextDouble();
+
+            if (nota != -1){
+                mediaEvaluaciones += nota;
+                totalEvaluaciones++;
+            }
+        }
+        System.out.println("La Media de evaluaciones para Matrix es: " + mediaEvaluaciones / totalEvaluaciones);
+    }
+}
